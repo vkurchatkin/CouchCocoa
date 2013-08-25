6b3d1a84be6b2bb71da09cf70d77c5193df78c14
Vladimir Kurchatkin <vladimir.kurchatkin@gmail.com>
2013-08-25 20:23:25 +0400

    Added some methods to CouchSequence

--- Couch/CouchSequence.h
+++ Couch/CouchSequence.h
@@ -8,5 +8,9 @@
 @property (readonly, nonatomic) BOOL ordered;
 
 - (BOOL) isLessOrEqual: (CouchSequence *) sequence;
+- (BOOL) isLessThen: (CouchSequence *) sequence;
+- (BOOL) isGreaterOrEqual: (CouchSequence *) sequence;
+- (BOOL) isGreaterThen: (CouchSequence *) sequence;
+- (BOOL) isEqualToSequence: (CouchSequence *) sequence;
 
 @end
--- Couch/CouchSequence.m
+++ Couch/CouchSequence.m
@@ -1,11 +1,40 @@
 #import "CouchSequence.h"
 
+@implementation CouchSequence
 
-@implementation CouchSequence {
-
-}
 - (id)initWithObject:(id)object {
     return nil;
+}
+
+- (BOOL)ordered {
+    return NO;
+}
+
+- (BOOL)isLessOrEqual:(CouchSequence *)sequence {
+    return NO;
+}
+
+- (BOOL)isLessThen:(CouchSequence *)sequence {
+    return NO;
+}
+
+- (BOOL)isGreaterOrEqual:(CouchSequence *)sequence {
+    return NO;
+}
+
+- (BOOL)isGreaterThen:(CouchSequence *)sequence {
+    return NO;
+}
+
+- (BOOL)isEqualToSequence:(CouchSequence *)sequence {
+    return NO;
+}
+
+- (BOOL)isEqualTo:(id)sequence {
+    if (![sequence isKindOfClass:[CouchSequence class]])
+        return NO;
+
+    return [self isEqualToSequence:sequence];
 }
 
 @end
